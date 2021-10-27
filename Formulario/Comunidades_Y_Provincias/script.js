742f2c9050028bf60d0c5e4790074126fed99510
romera <romera02rrs@gmail.com>
2021-10-27 13:51:44 +0200

    Formularios

---
+++ Formulario/Comunidades_Y_Provincias/script.js
@@ -0,0 +1,11 @@
+window.onload = main;
+
+function main(){
+
+    document.getElementById("enviar").addEventListener("click", procesarFormulario);;
+    cargaComunidades();
+}
+
+function cargaComunidades(){
+
+}
