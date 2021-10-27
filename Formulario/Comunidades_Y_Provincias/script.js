c045bc99507557fba3f3bfa8d794173b604c89ef
= <=>
2021-10-27 15:37:26 +0200

    Comunidades y provincias accede a las comunidades y las muestra en un select

--- Formulario/Comunidades_Y_Provincias/script.js
+++ Formulario/Comunidades_Y_Provincias/script.js
@@ -8,4 +8,35 @@ function main(){
 
 function cargaComunidades(){
 
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET', 'data.json', true);
+
+    xhttp.send();
+
+    xhttp.onreadystatechange = function(){
+
+        if(this.readyState == 4 && this.status == 200){
+
+            let datos = JSON.parse(this.responseText);
+
+            /*----------------------------------------------------------------------------------------------------------------*/
+
+            var selectForm = document.getElementById("comunidadesId");
+            var aux = "<option disabled selected>Selecciona una comunidad</option>";
+
+            for(let item of datos){
+                
+                aux += "<option value='"+item.comunitat+"'>"+item.comunitat+"</option>";
+            }
+            selectForm.innerHTML = aux;
+            
+            /*----------------------------------------------------------------------------------------------------------------*/
+
+        }
+    }
+}
+
+function procesarFormulario(){
+
 }
