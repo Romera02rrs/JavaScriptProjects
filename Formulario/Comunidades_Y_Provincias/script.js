c2b64c5d6f4309ed2f17055f81cb073cb48db3c4
= <=>
2021-10-29 15:29:31 +0200

    Comunidades y Provincias con uso de DOM terminado y comentado

--- Formulario/Comunidades_Y_Provincias/script.js
+++ Formulario/Comunidades_Y_Provincias/script.js
@@ -43,43 +43,30 @@ function cargaComunidades(){
 
 function cargaProvincias(){
 
-    var provincia = document.getElementById("comunidadesId").value;
-    var provinciasDiv = document.getElementById("provinciasDiv");
+    var provincia = document.getElementById("comunidadesId").value;                         // Obtengo la comunidad del select de las comunidades
+    var provSelectDiv = document.getElementById("provinciasSelectDiv");                     // Obtenfo el div donde se encuetra unicamente el select de provincias
 
-    document.getElementById("provinciasDiv").removeChild(document.getElementById("provinciasId"));
-    document.getElementById("provinciasDiv").removeChild(document.getElementById("provinciasDiv").firstChild);
-
-    var labelFormProv = document.createElement("label");
-    labelFormProv.appendChild(document.createTextNode("Selecciona su provincia"));
-    labelFormProv.setAttribute("for", "provinciasID");
-
-    var selectFormProv = document.createElement("select");
+    var selectFormProv = document.createElement("select");                                  // Creo el select y le añado sus atributos
     selectFormProv.setAttribute("id", "provinciasId");
     selectFormProv.setAttribute("name", "provincias");
 
-    var optionSelected = document.createElement("option");
+    var optionSelected = document.createElement("option");                                  // Creo una opcion que será la primera opción ya seleccionada y que esta desactivada
     optionSelected.appendChild(document.createTextNode("Seleccione una provincia"));
     optionSelected.setAttribute("selected", true);
     optionSelected.setAttribute("disabled", true);
-    selectFormProv.appendChild(optionSelected);
+    selectFormProv.appendChild(optionSelected);                                             // Inserto la opción desactivada al select
 
-    for(let item of datos){
-        if (item.comunitat == provincia){
-            for(let prov of item.provincies){
-                
-                var option = document.createElement("option");
+    for(let item of datos){                                                                 // Bucle que recorre todas las comunidades
+        if (item.comunitat == provincia){                                                   // Si encuentra la comunidad que he obtenido del select de comunidades
+            for(let prov of item.provincies){                                               // Ralizo un bucle que recorre todas las provincias de la comunidad encontrada
+                var option = document.createElement("option");                              // Creo opciones y las inserto en el select
                 option.appendChild(document.createTextNode(prov));
                 option.setAttribute("value", prov);
                 selectFormProv.appendChild(option);
             }
         }
     }
-    
-    provinciasDiv.appendChild(labelFormProv);
-    provinciasDiv.appendChild(selectFormProv);
 
-    console.clear();
-    console.log(provinciasDiv);
-
-    provinciasDiv.appendChild(selectFormProv);
+    provinciasDiv.appendChild(selectFormProv);                                              // Inserto en el div el select
+    provSelectDiv.replaceChildren(selectFormProv, selectFormProv);                          // Reemplazo el select del div
 }
