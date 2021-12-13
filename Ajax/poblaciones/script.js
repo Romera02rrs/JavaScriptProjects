51493131f3243f64b3b08d8a87b63856607cfdf0
Romera02rrs <Romera02rrs@gmail.com>
2021-12-13 09:57:25 +0100

    Ajax poblaciones

--- Ajax/poblaciones/script.js
+++ Ajax/poblaciones/script.js
@@ -1,15 +1,51 @@
-var respuesta;
+let poblacionsCV =[];
 
+window.onload = main;
+
+function main(){
+
+    document.getElementById("btn").addEventListener("click", carga);;
+}
+
+function carga(){
+
+    cargarPolaciones('03');
+
+    cargarPolaciones('12');
+
+    cargarPolaciones('46');
+
+    completar();
+}
+
+
+function cargarPolaciones (prov){
 xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function () {
     if (this.readyState==4 && this.status==200){
-        respuesta = JSON.parse(this.responseText);
-        console.log(respuesta);
+        let respuesta = JSON.parse(this.responseText);
+       
+        respuesta.data.forEach(element => {
+            poblacionsCV.push(element.DMUN50);
+        });
+        cambiaNumeroTotal(poblacionsCV.length);
     }
 }
-xmlhttp.open("GET","https://apiv1.geoapi.es/municipios?CPRO=03&type=JSON&key=&sandbox=1", true);
+xmlhttp.open("GET","https://apiv1.geoapi.es/municipios?CPRO="+prov+"&type=JSON&key=&sandbox=1", true);
 xmlhttp.send();
+}
 
-for (var item in respuesta) {
-    console.log(item);
+function cambiaNumeroTotal(num){
+
+    let poblaciones = document.getElementById("numeroPoblaciones");
+    poblaciones.innerHTML = num;
+}
+
+function completar(){
+    $( function() {
+        $( "#tags" ).autocomplete({
+          source: poblacionsCV
+        });
+      } );
+    
 }
