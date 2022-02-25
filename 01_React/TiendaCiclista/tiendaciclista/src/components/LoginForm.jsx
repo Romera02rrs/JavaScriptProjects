53f69b8b833309ae509ced3f4198022c83144285
Romera02rrs <Romera02rrs@gmail.com>
2022-02-25 13:54:21 +0100

    commit

--- 01_React/TiendaCiclista/tiendaciclista/src/components/LoginForm.jsx
+++ 01_React/TiendaCiclista/tiendaciclista/src/components/LoginForm.jsx
@@ -1,45 +1,31 @@
-import Button from "react-bootstrap/Button"
 import { Formik, Form, Field } from "formik"
 import * as Yup from 'yup'
 
 const SignupSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email').required('Required'),
-    password: Yup.string().min(6, 'Too Short!').max(20, 'Too Long!').required('Required')
+    email: Yup.string().email('Email inválido').min(6, 'Demasiado corto!').max(255, 'Demasiado Largo!').required('Required'),
+    password: Yup.string().min(6, 'Demasiado corto!').max(20, 'Demasiado largo!').required('Required')
 })
 
-const LoginForm = () => {
+const regForm = () => {
     return (
         <div className="container">
             <div className="row">
                 <div className="col-lg-12">
-                    <Formik initialValues={{ email: "", password: "" }}
-                        validationSchema={SignupSchema}
-                        onSubmit={values => { console.log(values); }}>
+                    <Formik initialValues={{ email: "", password: "" }} validationSchema={SignupSchema}>
                         {({ errors, touched }) => (
                             <Form>
-                                <div className="mb-3 row">
-                                    <label htmlFor="email" className="col-sm-2 col-form-label">Correo electrónico</label>
+                                <div className="mb-3">
+                                    <label htmlFor="regFormEmail" className="form-label">Correo electrónico</label>
                                     <div className="col-sm-10">
-                                        <Field
-                                            type="email"
-                                            name="email"
-                                            placeholder="Enter email"
-                                            autoComplete="off"
-                                            className="Form-control" />
-                                        {errors.email && touched.email ?
-                                            <div>{errors.email}</div> : null}
+                                        <Field type="email" id="regFormEmail" name="email" placeholder="Introduce el email" autoComplete="off" className="form-control" />
+                                        {errors.email && touched.email ? <div className="text-danger">{errors.email}</div> : null}
                                     </div>
                                 </div>
-                                <div className="mb-3 row">
-                                    <label htmlFor="password" className="col-sm-2 col-form-label">Password</label>
+                                <div className="mb-3">
+                                    <label htmlFor="regFormPassword" className="form-label">Contraseña</label>
                                     <div className="col-sm-10">
-                                        <Field
-                                            type="password"
-                                            name="password"
-                                            placeholder="Enter password"
-                                            className="Form-control" />
-                                        {errors.password && touched.password ?
-                                            <div>{errors.password}</div> : null}
+                                        <Field type="password" id="regFormPassword" name="password" placeholder="Introduce la contraseña" className="form-control" />
+                                        {errors.password && touched.password ? <div className="text-danger">{errors.password}</div> : null}
                                     </div>
                                 </div>
                                 <button type="submit" className="btn btn-primary btn-block mt-4">Enviar</button>
@@ -52,4 +38,4 @@ const LoginForm = () => {
     )
 }
 
-export default LoginForm
+export default regForm
--- 01_React/TiendaCiclista/tiendaciclista/src/components/RegForm.jsx
+++ 01_React/TiendaCiclista/tiendaciclista/src/components/RegForm.jsx
@@ -1,38 +1,124 @@
-import Form from "react-bootstrap/Form"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
-import Button from "react-bootstrap/Button"
+import { Formik, Form, Field } from "formik"
+import * as Yup from 'yup'
+
+const RegisterSchema = Yup.object().shape({
+    name: Yup.string().min(6, 'Demasiado corto!').max(255, 'Demasiado Largo!').required('Required'),
+    email: Yup.string().email('Email inválido').min(6, 'Demasiado corto!').max(255, 'Demasiado Largo!').required('Required'),
+    password: Yup.string().min(6, 'Demasiado corto!').max(20, 'Demasiado largo!').required('Required'),
+    passwordConfirmation: Yup.string("").oneOf([Yup.ref('password'), null], 'No coincide').required('Required')
+})
 
 const RegForm = () => {
     return (
-        <Form>
-            <Form.Group className="mb-3" controlId="formName">
-                <Form.Label>Nombre</Form.Label>
-                <Form.Control type="text" placeholder="Introducir nombre" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formEmail">
-                <Form.Label>Correo electrónico</Form.Label>
-                <Form.Control type="email" placeholder="introducir email" />
-            </Form.Group>
-
-            <Row className="mb-3">
-                <Form.Group as={Col} controlId="formPasswd">
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control type="password" placeholder="Contraseña" />
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="formPasswdRepited">
-                    <Form.Label>Repetir contraseña</Form.Label>
-                    <Form.Control type="password" placeholder="Repetir Contraseña" />
-                </Form.Group>
-            </Row>
-
-            <Button variant="primary" type="submit">
-                Registrar
-            </Button>
-        </Form>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <Formik initialValues={{ name: "", email: "", password: ""}} 
+                            validationSchema={RegisterSchema}
+                            onSubmit={values => {
+                                registrar(values)
+                            }}>
+                        {({ errors, touched }) => (
+                            <Form>
+                                <div className="mb-3">
+                                    <label htmlFor="regFormName" className="form-label">Nombre</label>
+                                    <div className="col">
+                                        <Field type="text" id="regFormName" name="name" placeholder="Introducir nombre" autoComplete="off" className="form-control" />
+                                        {errors.name && touched.name ? <div className="text-danger">{errors.name}</div> : null}
+                                    </div>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="loginFormEmail" className="form-label">Correo electrónico</label>
+                                    <div className="col">
+                                        <Field type="email" id="loginFormEmail" name="email" placeholder="Introduce el email" autoComplete="off" className="form-control" />
+                                        {errors.email && touched.email ? <div className="text-danger">{errors.email}</div> : null}
+                                    </div>
+                                </div>
+                                <div className="row mb-3">
+                                    <div className="col">
+                                        <label htmlFor="loginFormPassword" className="form-label">Contraseña</label>
+                                        <div className="col">
+                                            <Field type="password" id="loginFormPassword" name="password" placeholder="Introduce la contraseña" className="form-control" />
+                                            {errors.password && touched.password ? <div className="text-danger">{errors.password}</div> : null}
+                                        </div>
+                                    </div>
+                                    <div className="col">
+                                        <label htmlFor="loginFormPasswordConfirmation" className="form-label">Confirmar contraseña</label>
+                                        <div className="col">
+                                            <Field type="password" id="loginFormPasswordConfirmation" name="passwordConfirmation" placeholder="Repite la contraseña" className="form-control" />
+                                            {errors.passwordConfirmation && touched.passwordConfirmation ? <div className="text-danger">{errors.passwordConfirmation}</div> : null}
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="submit" className="btn btn-primary btn-block mt-4">Enviar</button>
+                            </Form>
+                        )}
+                    </Formik>
+                </div>
+            </div>
+        </div>
     )
 }
 
+function registrar(values){
+
+    const usuario = {
+        name : values.name,
+        email: values.email,
+        password: values.password
+    }
+
+    fetch("https://api.tendaciclista.ccpegoilesvalls.es/api/register", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(usuario)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        if(data.error == null){
+            alert("Correcto")
+            // window.location.href = "llistatLlibres.html"
+        }else{
+            alert("ERROR")
+            //error2(document.getElementById("nom"), data.error)
+        }
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
 export default RegForm
+
+// return (
+//     <Form>
+//         <Form.Group className="mb-3" controlId="formName">
+//             <Form.Label>Nombre</Form.Label>
+//             <Form.Control type="text" placeholder="Introducir nombre" />
+//         </Form.Group>
+
+//         <Form.Group className="mb-3" controlId="formEmail">
+//             <Form.Label>Correo electrónico</Form.Label>
+//             <Form.Control type="email" placeholder="introducir email" />
+//         </Form.Group>
+
+//         <Row className="mb-3">
+//             <Form.Group as={Col} controlId="formPasswd">
+//                 <Form.Label>Contraseña</Form.Label>
+//                 <Form.Control type="password" placeholder="Contraseña" />
+//             </Form.Group>
+
+//             <Form.Group as={Col} controlId="formPasswdRepited">
+//                 <Form.Label>Repetir contraseña</Form.Label>
+//                 <Form.Control type="password" placeholder="Repetir Contraseña" />
+//             </Form.Group>
+//         </Row>
+
+//         <Button variant="primary" type="submit">
+//             Registrar
+//         </Button>
+//     </Form>
+// )
